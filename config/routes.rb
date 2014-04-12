5830587b5084324afb3c529c2b0fc72299c7a97f
Lulu <ella516@qq.com>
2014-04-12 19:09:54 +0200

    finish demo app

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ DemoApp::Application.routes.draw do
   resources :microposts
 
   resources :users
-
+  root  'users#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
